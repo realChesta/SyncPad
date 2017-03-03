c081a1414dbbe504dfb0504ea86a9069bf9a04d6
Chester <chesta@kekt.us>
2017-03-03 17:44:15 +0100

    Reformatted code, updated settings

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './syncpad-icon.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to SyncPad</h2>
-        </div>
-        <p className="App-intro">
-          To get started, choose an existing session from the list or create a new one below.
-        </p>
-      </div>
-    );
-  }
+    render()
+    {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to SyncPad</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, choose an existing session from the list or create a new one below.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
