baec0af9f45f06b2ec4c54d10ef45d1b2d176f97
Kirill <kirill.u@kekt.us>
2017-03-14 22:47:22 +0100

    Testing Sockets

--- server/src/main.js
+++ server/src/main.js
@@ -13,17 +13,16 @@ function generateGUID() {
 };
 
 var sessionList = {
-    id1: {name: "Krek1", content: {}, users: ["Krekosaurus", "Adolf", "Merkel"]},
-    id2: {name: "Sample2", content: {}, users: ["Loner"]},
-    id3: {name: "Max Muster", content: {}, users: ["Fax Fuster","Dax Duster"]}
+    "Krek1": {session: "Krek1", content: {}, users: ["Krekosaurus", "Adolf", "Merkel"]},
+    "Sample2": {session: "Sample2", content: {}, users: ["Loner"]},
+    "Max Muster": {session: "Max Muster", content: {}, users: ["Fax Fuster", "Dax Duster"]}
 };
 
 function getInfo() {
     let listSessions = [];
     for (let session in sessionList) {
         let obj = {
-            id: session,
-            name: sessionList[session].name,
+            sessionName: sessionList[session].name,
             content: sessionList[session].content,
             users: sessionList[session].users.length
         };
@@ -33,7 +32,26 @@ function getInfo() {
     return listSessions;
 }
 
-app.use(function(req, res, next) {
+function checkSession(usr) {
+    for (let session in sessionList) {
+        if (usr.session === session.sessionName) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkUser(session) {
+    for (let user in session.users) {
+        if (session.name === user) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -52,22 +70,30 @@ app.listen(80, function () {
     console.log("Krekosaurus");
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-});
-
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
+    });
+    socket.on('auth', function (usr) {
+        if (!checkSession(usr)) {
+            sessionList[usr.session] = {
+                sessionName: usr.session,
+                content: '',
+                users: [usr.name]
+            };
+        }
+        else {
+            if (checkUser(usr)) {
+                console.log("Occupied");
+            }
+            else {
+                sessionList[usr.session].users.push(usr.name);
+            }
+        }
     });
 });
 
-io.on('auth', function(socket) {
-    socket.on('auth', function(usr){
-        if (usr.name =)
-    })
-})
+io.on('auth', function (socket) {
 
-io.on('join', function(socket) {
-})
+});
