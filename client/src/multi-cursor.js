81fd186218b14394bbebe5977e5d6127058956e3
Chester <chesta@kekt.us>
2017-03-27 01:02:47 +0200

    Reformatted multi-cursor.js

--- client/src/multi-cursor.js
+++ client/src/multi-cursor.js
@@ -3,7 +3,8 @@
  */
 
 class MultiCursor {
-    constructor(quill, options) {
+    constructor(quill, options)
+    {
         let defaults = {
             template: `<span class="ql-cursor-flag">
                <span class="ql-cursor-name"></span>
@@ -19,17 +20,21 @@ class MultiCursor {
         quill.on('text-change', this.applyDelta.bind(this));
     }
 
-    clearCursors() {
+    clearCursors()
+    {
         Object.keys(this.cursors).forEach(this.removeCursor.bind(this));
     }
 
-    moveCursor(userId, index) {
+    moveCursor(userId, index)
+    {
         var cursor = this.cursors[userId];
-        if(cursor) {
+        if (cursor)
+        {
             cursor.index = index;
             cursor.elem.classList.remove('ql-hidden');
             clearTimeout(cursor.timer);
-            cursor.timer = setTimeout(() => {
+            cursor.timer = setTimeout(() =>
+            {
                 cursor.elem.classList.add('ql-hidden');
                 cursor.timer = null;
             }, this.options.timeout);
@@ -38,56 +43,74 @@ class MultiCursor {
         }
     }
 
-    removeCursor(userId) {
+    removeCursor(userId)
+    {
         var cursor = this.cursors[userId];
-        if(cursor) {
+        if (cursor)
+        {
             cursor.elem.parentNode.removeChild(cursor.elem);
         }
         delete this.cursors[userId];
     }
 
-    setCursor(userId, index, name, color) {
-        if(!this.cursors[userId]) {
+    setCursor(userId, index, name, color)
+    {
+        if (!this.cursors[userId])
+        {
             var cursor = {
                 userId: userId,
                 index: index,
                 color: color,
                 elem: this.buildCursor(name, color)
-            }
+            };
             this.cursors[userId] = cursor;
         }
-        setTimeout(() => {
+        setTimeout(() =>
+        {
             this.moveCursor(userId, index);
         }, 1);
         return this.cursors[userId];
     }
 
-    shiftCursors(index, length, authorId = null) {
-        Object.values(this.cursors).forEach((cursor) => {
+    shiftCursors(index, length, authorId = null)
+    {
+        Object.values(this.cursors).forEach((cursor) =>
+        {
             var shift = Math.max(length, index - cursor.index);
-            if(cursor.userId == authorId) {
+            if (cursor.userId == authorId)
+            {
                 this.moveCursor(authorId, cursor.index + shift);
-            } else if(cursor.index > index) {
+            }
+            else if (cursor.index > index)
+            {
                 cursor.index += shift;
             }
         });
     }
 
-    update() {
+    update()
+    {
         Object.values(this.cursors).forEach(this.updateCursor.bind(this));
     }
 
-    applyDelta(delta) {
+    applyDelta(delta)
+    {
         var index = 0;
-        delta.ops.forEach((op) => {
+        delta.ops.forEach((op) =>
+        {
             var length = 0;
-            if (op.insert) {
+            if (op.insert)
+            {
                 length = op.insert.length || 1;
                 var author = op.attributes ? op.attributes.author : null;
                 this.shiftCursors(index, length, author);
-            } else if(op.delete) {
-                this.shiftCursors(index, -1*op.delete, null);
-            } else if(op.retain) {
+            }
+            else if (op.delete)
+            {
+                this.shiftCursors(index, -1 * op.delete, null);
+            }
+            else if (op.retain)
+            {
                 this.shiftCursors(index, 0, null);
                 length = op.retain;
             }
@@ -96,7 +119,8 @@ class MultiCursor {
         this.update();
     }
 
-    buildCursor(name, color) {
+    buildCursor(name, color)
+    {
         var cursorEl = document.createElement('span');
         cursorEl.classList.add('ql-cursor');
         cursorEl.innerHTML = this.options.template;
@@ -110,9 +134,11 @@ class MultiCursor {
         return cursorEl;
     }
 
-    updateCursor(cursor) {
+    updateCursor(cursor)
+    {
         var bounds = this.quill.getBounds(cursor.index);
-        if(bounds) {
+        if (bounds)
+        {
             cursor.elem.style.top = (bounds.top + this.quill.container.scrollTop) + 'px';
             cursor.elem.style.left = bounds.left + 'px';
             cursor.elem.style.height = bounds.height + 'px';
@@ -120,7 +146,9 @@ class MultiCursor {
             cursor.elem.classList.toggle('ql-top', parseInt(cursor.elem.style.top) <= flag.offsetHeight);
             cursor.elem.classList.toggle('ql-left', parseInt(cursor.elem.style.left) <= flag.offsetWidth);
             cursor.elem.classList.toggle('ql-right', this.quill.root.offsetWidth - parseInt(cursor.elem.style.left) <= flag.offsetWidth);
-        } else {
+        }
+        else
+        {
             this.removeCursor(cursor.userId);
         }
     }
