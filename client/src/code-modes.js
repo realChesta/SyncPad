814148f2fd98cee9fd385d21144c12e8ab725197
Chester <chesta@kekt.us>
2017-03-28 16:26:47 +0200

    Started implementing code modes for the ace editor

---
+++ client/src/code-modes.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Kyrill on 28.03.2017.
+ */
+
+import 'brace/mode/c_cpp';
+import 'brace/mode/coffee';
+import 'brace/mode/csharp';
+import 'brace/mode/css';
+import 'brace/mode/html';
+import 'brace/mode/java';
+import 'brace/mode/javascript'
+import 'brace/mode/json';
+import 'brace/mode/jsx';
+import 'brace/mode/latex';
+import 'brace/mode/lua';
+import 'brace/mode/markdown';
+import 'brace/mode/objectivec';
+import 'brace/mode/php';
+import 'brace/mode/python';
+import 'brace/mode/sass';
+import 'brace/mode/scss';
+import 'brace/mode/sql';
+import 'brace/mode/swift';
+import 'brace/mode/tex';
+import 'brace/mode/typescript';
+import 'brace/mode/xml';
+import 'brace/mode/yaml';
+import 'brace/mode/plain_text';
+import 'brace/mode/text';
+
+function getModeValues()
+{
+
+}
